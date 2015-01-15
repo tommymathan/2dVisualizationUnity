7737edc8fec1f7fa34d9fd0d9ade5da8b8466414
Thomassc <thomassc@cwu.edu>
2015-01-14 20:17:58 -0800

    Added Support for multiple vectors/colors

--- New Unity Project/Assets/Scripts/ShiftedCordinateDrawer.cs
+++ New Unity Project/Assets/Scripts/ShiftedCordinateDrawer.cs
@@ -8,24 +8,47 @@ public class ShiftedCordinateDrawer : MonoBehaviour
 		private static int ANIMATIONSPEED = 10; //Determines the number of frames it takes to render a segment of the line
 
 		static Material lineMaterial;
-		static List<float> givenData;
 		static float givenXMax;
 		static float givenYMax; // x and y max are used to determine camera perspective size
 		static float camOrtho;  // camera ortho will tell the camera how much area to display in Unity units
 		private float orginX;
 		private float orginY;
+		private List <Color32> col;
+
+
+		//////////////////////////////////////Debug Variables/////////////////
+		private List <List <float>> dataSet = new List <List <float>> ();
 		private int currentFrame;
 		public GUIText currentFText;
+		static List<float> givenData;
+
+		//////////////////////////////////////////////////////////////////////	
+
 
 
 		// Initalize camera and values
 		void Start ()
 		{
-				//setup list
+				////////////////////////Debug code///////////////////////
 				givenData = new List<float> (new float[] {0,0,1,1,2,4,3,1,10,1});
-		for (int i = 0; i < 100; i++) {
-			givenData.Add(Random.value);
+				for (int i = 0; i < 100; i++) {
+						givenData.Add (Random.value);
 				}
+
+				List<float> dataList1 = new List<float> (new float[] {0,0,1,1,2,4,3,1,10,1});
+				List<float> dataList2 = new List<float> (new float[] {0,1,2,3,4,5,6,3,14,6});
+				List<float> dataList3 = new List<float> (new float[] {0,3,5,3,3,1,6,1,2,8});
+				List<float> dataList4 = new List<float> (new float[] {1,1,7,3,4,2,5,3,7,3});
+
+				
+				dataSet.Add (dataList1);
+				dataSet.Add (dataList2);
+				dataSet.Add (dataList3);
+				dataSet.Add (dataList4);
+				////////////////////////////////////////////////////////////////////////
+			
+				col = new List<Color32> ();
+				setColorsForNumberOfVectors ();				
 				currentFrame = 0;
 				currentFText.text = "Current Frame: " + currentFrame.ToString ();
 				GatherMaxValues ();		
@@ -36,7 +59,7 @@ public class ShiftedCordinateDrawer : MonoBehaviour
 
 
 		}
-	
+
 		// Update is called once per frame
 		void Update ()
 		{
@@ -57,13 +80,26 @@ public class ShiftedCordinateDrawer : MonoBehaviour
 		//Drawing of lines is done in this method
 		void OnPostRender ()
 		{
+
+
 				beginDraw ();
 				drawGridLines ();
-				animatedDrawShiftedPair ();
+				splitAndDrawIndividualVectors ();
 				//End drawing
 				GL.End ();
 		}
 
+		void splitAndDrawIndividualVectors ()
+		{
+				int colorIndex = 0;
+				foreach (List<float> vector in dataSet) {		
+						animatedDrawShiftedPair (vector, col [colorIndex]);
+						colorIndex++;
+				}
+		
+	
+
+		}
 		//Set up drawing area, create the line material then detemine the drawing shape then color
 		void beginDraw ()
 		{
@@ -78,10 +114,10 @@ public class ShiftedCordinateDrawer : MonoBehaviour
 		//draws a standard line representation of the data
 		void drawLine (int cursor)
 		{
-						for (int i = 0; i < cursor; i+=2) {
-								GL.Vertex3 (givenData [i], givenData [i + 1], 0f);
-								GL.Vertex3 (givenData [i + 2], givenData [i + 3], 0f);
-						}
+				for (int i = 0; i < cursor; i+=2) {
+						GL.Vertex3 (givenData [i], givenData [i + 1], 0f);
+						GL.Vertex3 (givenData [i + 2], givenData [i + 3], 0f);
+				}
 				
 
 		
@@ -89,6 +125,7 @@ public class ShiftedCordinateDrawer : MonoBehaviour
 		//Draws gridlines so that we can debug more easily, draw in grey then switch back to white
 		void drawGridLines ()
 		{
+
 				GL.Color (Color.grey);
 		
 				for (int i = 0; i < (int)Mathf.Ceil(givenXMax); i++) {
@@ -103,8 +140,7 @@ public class ShiftedCordinateDrawer : MonoBehaviour
 				GL.Color (Color.white);
 		}
 	
-		//draws a shifted pair representation of the line 
-		//Possible ways of animating this method, using a separate thread and a semaphore,
+		//draws a shifted pair representation of the line
 		//re-writing to draw small number of lines at a time
 
 		void drawShiftedPair (int cursor)
@@ -129,11 +165,35 @@ public class ShiftedCordinateDrawer : MonoBehaviour
 						GL.Begin (GL.LINES);
 						GL.Color (Color.white);
 
-						//Semaphore goes here
 				}
 		
 		}
-	
+
+		void drawShiftedPair (int cursor, List<float> currentVector, Color c)
+		{
+		
+				orginX = 0;
+				orginY = 0;
+		
+				for (int i = 0; i < cursor; i+=2) {
+						orginX = currentVector [i] + orginX;
+						orginY = currentVector [i + 1] + orginY;
+			
+						GL.Vertex3 (orginX, orginY, 0f);
+						GL.Vertex3 ((currentVector [i + 2] + orginX), (currentVector [i + 3] + orginY), 0f);
+			
+						Vector3 point1 = new Vector3 (orginX, orginY, 0f);
+						Vector3 point2 = new Vector3 ((currentVector [i + 2] + orginX), (currentVector [i + 3] + orginY), 0f);
+			
+						//Experimental code attempting to draw arrows
+						GL.End ();
+						drawArrowHead (point1, point2);
+						GL.Begin (GL.LINES);
+						GL.Color (c);
+
+				}
+		
+		}
 		//Tony's arrow drawing method 
 		void drawArrowHead (Vector3 point1, Vector3 point2)
 		{
@@ -175,18 +235,26 @@ public class ShiftedCordinateDrawer : MonoBehaviour
 		}
 /////////////////////////UTILITY Non-Drawing FUNCTIONS///////////////////////////////////////////
 	
-	//Draw the shifted pair graph at 1 line per ANIMATIONSPEED frames(E.g. 1 line per 10 frames)
-	//To do this we draw partial graphs untill we have the whole graph
-	void animatedDrawShiftedPair(){
-		
-		if ((currentFrame / ANIMATIONSPEED) < givenData.Count - 2) {
-			drawShiftedPair (currentFrame / ANIMATIONSPEED);
-		} else
-			drawShiftedPair (givenData.Count - 2);
-		
-	}
+		//Draw the shifted pair graph at 1 line per ANIMATIONSPEED frames(E.g. 1 line per 10 frames)
+		//To do this we draw partial graphs untill we have the whole graph
+		void animatedDrawShiftedPair ()
+		{
+				if ((currentFrame / ANIMATIONSPEED) < givenData.Count - 2) {
+						drawShiftedPair (currentFrame / ANIMATIONSPEED);
+				} else
+						drawShiftedPair (givenData.Count - 2);		
+		}
 
-
+		void animatedDrawShiftedPair (List<float> currentVector, Color c)
+		{	
+				if (c != null)
+						GL.Color (c);
+				if ((currentFrame / ANIMATIONSPEED) < currentVector.Count - 2) {
+						drawShiftedPair (currentFrame / ANIMATIONSPEED, currentVector, c);
+				} else
+						drawShiftedPair (currentVector.Count - 2, currentVector, c);	
+				GL.Color (Color.white);
+		}
 
 		static void CreateLineMat ()
 		{
@@ -204,8 +272,8 @@ public class ShiftedCordinateDrawer : MonoBehaviour
 				lineMaterial.shader.hideFlags = HideFlags.HideAndDontSave;
 		}
 
-	//Find the maximum x and y that we will reach durring the shifted pairs by adding all x and y values together
-	//The sum of each axis's values represents the maximum value for each axis
+		//Find the maximum x and y that we will reach durring the shifted pairs by adding all x and y values together
+		//The sum of each axis's values represents the maximum value for each axis
 		void GatherMaxValues ()
 		{
 				givenXMax = 0;
@@ -223,6 +291,16 @@ public class ShiftedCordinateDrawer : MonoBehaviour
 				Debug.Log ("The max X value in this list is: " + givenXMax + "" +
 						"\nThe max Y value in this list is: " + givenYMax);
 		}
-	
+
+	//Creates random colors for each vector, may have to increase the variance if too many vectors are the
+	//same color
+	void setColorsForNumberOfVectors ()
+	{
+		for (int i = 0; i < dataSet.Count; i++) {
+			Random.Range (0, 1);
+			col.Add (new Color (Random.value, Random.value, 
+			                    Random.value, 255));
+		}
+	}
 
 }
