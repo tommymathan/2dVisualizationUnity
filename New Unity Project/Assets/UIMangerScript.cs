7a3669666d753cc6fe6d6f3b6dca991b2386109a
Thomassc <thomassc@cwu.edu>
2015-01-29 23:46:24 -0800

    Revert "Revert "Fixed GUI Animations, Added functionality to some""
    This reverts commit 318483a7340318e1c90a737fee18949622600201.

--- New Unity Project/Assets/UIMangerScript.cs
+++ New Unity Project/Assets/UIMangerScript.cs
@@ -1,8 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
 
 public class UIMangerScript : MonoBehaviour {
 
+	//Location of file selected
+	private string address;
+
+	/*
+	 * Open button clicked and Open dialog pops up
+	 */
+	public void openButtonClicked()
+	{
+		address = EditorUtility.OpenFilePanel ("Open File", "", "csv");
+	}
+
+	/*
+	 * Returns the address or location of the file selected
+	 */
+	public string getAddress()
+	{
+		return address;
+	}
+
+	/*
+	 * Exits application when the exit button is clicked
+	 */
+	public void exitButtonClicked()
+	{
+		Application.Quit ();
+	}
 
 	//set gameobject to do an Animation
 	void animOpenFile(GameObject g, string anim, int speed)
