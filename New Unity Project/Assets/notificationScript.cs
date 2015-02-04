361aa077178b001a634b87d12a212ee1e9f5cab1
tony2b2 <tonyaa2007@gmail.com>
2015-02-03 22:21:49 -0800

    Added Notification for Excel
    Has some functionality but not the ability to redraw the graphs

---
+++ New Unity Project/Assets/notificationScript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+
+public class notificationScript : MonoBehaviour {
+
+	/*
+	 * Location of the unity file ---unity.csv---
+	 */
+	public string path = "C:/Users/Public/unity.csv";
+	System.DateTime time;
+	public string animation;
+
+	/**
+	 * Check for modification of the unity file
+	 */
+	void Start () {
+
+		//Checks if file exists if not creates it
+		if (!File.Exists (path))
+			File.Create (path);
+		else
+			time = File.GetLastWriteTime (path);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log ("Object is: " + gameObject.name);
+		//Get time and date of file 
+		System.DateTime modTime = File.GetLastWriteTime (path);
+
+		//Check to see if it has been modified
+		if (modTime.Day != time.Day || modTime.Hour != time.Hour || modTime.Minute != time.Minute || modTime.Second != time.Second) {
+			time = modTime;
+			animObj (gameObject, 1);
+		}
+
+	}
+	/**
+	 * Action for okay button
+	 */
+	public void notificationOkayButton()
+	{
+		//Animate object
+		animObj (gameObject, -1);
+
+		/**
+		 * Call data management to update
+		 */
+	}
+
+	void animObj(GameObject g, int speed)
+	{
+		g.animation [animation].speed = speed;
+		
+		//No queue of animation, Plays it now
+		g.animation [animation].weight = 1;
+		g.animation [animation].time = 0;
+		g.animation.Play ();
+
+		
+	}
+}
